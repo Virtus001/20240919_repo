8a5c5d9074795307f83ffdfb0024c1daeea520b3
virtus <zwbmemo@outlook.com>
2024-09-19 18:12:34 +0800

    first commit

---
+++ first/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+  std::cout<<"Here is developagain branch!"<<std::endl;
+  
+  std::string again = "again";
+  std::cout<<"again = "<<again<<std::endl;
+
+  return 0;
+}
